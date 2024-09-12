c870b8572dcd27989ce8b9892bddbe76977a2415
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-11 20:49:41 -0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+// pages/index.js
+import React from 'react';
+import PolicyForm from '../components/PolicyForm';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Policy Automation Tool</h1>
+      <PolicyForm />
+    </div>
+  );
+}
