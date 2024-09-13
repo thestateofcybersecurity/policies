04d9a1664fa188178b81f9e0fdd285c987d078e8
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-13 13:50:57 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
-// pages/_app.js
 import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css'; // Import the CSS for icons
-config.autoAddCss = false; // Prevent Font Awesome from adding its CSS automatically
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 import '../styles/globals.css'
 import Head from 'next/head';
 
