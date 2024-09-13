e7932bbeba54f3728e2d24f09483f8e963427c47
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-13 13:36:11 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 // pages/index.js
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClipboard, faCog } from '@fortawesome/free-solid-svg-icons';
 import '../styles/policy-styles.css';
 
 export default function Home() {
@@ -8,8 +10,20 @@ export default function Home() {
       <h1>Policy Management System</h1>
       <nav>
         <ul>
-          <li><Link href="/manage-templates"><a>Manage Templates</a></Link></li>
-          <li><Link href="/generate-policies"><a>Generate Policies</a></Link></li>
+          <li>
+            <Link href="/manage-templates">
+              <a>
+                <FontAwesomeIcon icon={faCog} /> Manage Templates
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/generate-policies">
+              <a>
+                <FontAwesomeIcon icon={faClipboard} /> Generate Policies
+              </a>
+            </Link>
+          </li>
         </ul>
       </nav>
     </div>
