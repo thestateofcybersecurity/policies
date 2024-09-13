04e8a51b3ff5bbbcd2e27f57ed1990771771d741
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-13 15:08:36 -0600

    Update PolicyTemplate.js

--- models/PolicyTemplate.js
+++ models/PolicyTemplate.js
@@ -1,11 +1,10 @@
-// models/PolicyTemplate.js
 import mongoose from 'mongoose';
 
 const PolicyTemplateSchema = new mongoose.Schema({
   name: { type: String, required: true },
   content: { type: String, required: true },
   type: { type: String, enum: ['html', 'text'], default: 'html' },
-  category: { type: String, enum: ['NIST CSF', 'ISO 27001'], required: true }
+  category: { type: String, enum: ['NIST CSF', 'ISO 27001', 'CIS IG1'], required: true }
 }, { timestamps: true });
 
 const PolicyTemplate = mongoose.models.PolicyTemplate || mongoose.model('PolicyTemplate', PolicyTemplateSchema);
