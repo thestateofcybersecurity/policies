b2d49b1239e1633971d321d061d3a3f034fef0dd
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-13 14:06:00 -0600

    Update manage-templates.js

--- pages/manage-templates.js
+++ pages/manage-templates.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEdit, faTrash, faSave, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faEdit, faTrash, faSave, faTimes, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 export default function ManageTemplates() {
   const [templates, setTemplates] = useState([]);
